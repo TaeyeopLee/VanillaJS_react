71219452c3a28d874d077e31c3e5321e223c3777
Lee Taeyeop <lee.taeyeop@hotmail.com>
2021-07-11 21:47:32 +0900

    searchForm2

--- js/views/FormView.js
+++ js/views/FormView.js
@@ -1,18 +1,26 @@
 import View from './View';
-import qs from '../helper';
+import qs, { on } from '../helper';
 
 class FormView extends View {
   constructor() {
-    super(qs("#form-view")) // 1: View 클래스 생성자 함수를 통해 돔을 내부 변수로 저장한다.
-    // qs()는 document.querySelector()를 호출하는 헬퍼 함수다.
+    super(qs("#form-view"))
 
-    this.inputElement = qs("[type=text]", this.element) // 2
-    this.resetElement = qs("[type=reset]", this.element) // 3
-    this.showResetButton(false) // 4: 처음에는 x 버튼을 보일 필요가 없기 때문에 숨김.
+    this.inputElement = qs("[type=text]", this.element)
+    this.resetElement = qs("[type=reset]", this.element)
+    this.showResetButton(false);
+    this.bindEvents() // 1: 뷰에서 발생하는 이벤트 처리기 연결용 bindEvents()메서드 호출
   }
-  // 5
+  
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
   }
-  // 4, 5: 화면의 노출 여부를 display속성을 통해 제어한다.
+
+  bindEvents() {
+    on(this.inputElement, "keyup", event => this.handleKeyup(event)) // 2: 미리 저장해 둔 inputElement에서 keyup 이벤트가 발생하면 handleKeyup() 메서드를 호출하는 함수를 연결.
+  }
+
+  handleKeyup(event) {
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0) // 3: 입력한 문자열의 길이에 따라 버튼의 노출 여부를 결정. 입력한 문자가 있으면 x 버튼을 표시하고 그렇지 않으면 숨기도록 구현.
+  }
 }
