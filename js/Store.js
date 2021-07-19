a29c966958b089f9ecf91ebbacc5378f5d87f66d
Lee Taeyeop <lee.taeyeop@hotmail.com>
2021-07-19 23:56:06 +0900

    history 3

--- js/Store.js
+++ js/Store.js
@@ -12,6 +12,21 @@ export default class Store {
   search(keyword) {
     this.searchKeyword = keyword;
     this.searchResult = this.storage.productData.filter((product) => product.name.includes(keyword));
+    this.addHistory(keyword); // 1: 검색 로직 마지막에 검색 키워드를 히스토리에 추가한다.
+  }
+
+  addHistory(keyword = "") {
+    keyword = keyword.trim();
+    if (!keyword) return;
+
+    // 2: 이미 있는 검색어일 경우
+    const hasHistory = this.storage.historyData.some(
+      history => history.keyword === keyword
+    )
+    if (hasHistory) this.removeHistory(keyword) // 3: 검색어를 삭제한다.
+
+    const date = new Date();
+    this.storage.historyData.push({ keyword, date }); // 4: 변경된 검색시간으로 다시 검색어를 추가한다.
   }
 
   getKeywordList() {
@@ -22,7 +37,6 @@ export default class Store {
     return this.storage.historyData.sort(this._sortHistory);
   }
 
-  // 1: 삭제할 키워드를 받아 데이터에서 제거하는 removeHistory
   removeHistory(keyword) {
     this.storage.historyData = this.storage.historyData.filter(
       history => history.keyword !== keyword
